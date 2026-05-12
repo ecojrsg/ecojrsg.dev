1f880550c840eeec89178e11c7b4e23d01248da7
ecojrsg <jonathan.rsg.serna@gmail.com>
2026-05-11 21:12:30 -0600

    feat: enhance AboutCard and LetsWork sections with internationalization support; update contact links and add Linkedin icon

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,8 +4,9 @@ export const Des: { [key: string]: string } = {
 
 export const LINKS = {
   github: "https://github.com/ecojrsg",
-  instagram: "https://www.instagram.com/eco_jrsg/",
+  instagram: "https://www.instagram.com/katzekingdom/",
   discord: "https://discord.gg/zGph7wce6z",
+  linkedin: "https://www.linkedin.com/in/jrsg/"
 };
 
 export const loaderAnimation = [
--- src/lib/navLinks.ts
+++ src/lib/navLinks.ts
@@ -1,8 +1,8 @@
 export const navLinks = [
     { href: "/#Inicio", i18nKey: "nav.home" },
     { href: "/#About", i18nKey: "nav.about" },
-    { href: "/", i18nKey: "nav.team" }, // Placeholder for now, will be updated
     { href: "/#Technology", i18nKey: "nav.skills" },
+    { href: "/", i18nKey: "nav.team" }, // Placeholder for now, will be updated
     { href: "/", i18nKey: "nav.contac" }, // Placeholder for now, will be updated
     // { href: "/blog", i18nKey: "nav.blog" },
     { href: "https://blog.ecojrsg.dev", i18nKey: "nav.blog" },
